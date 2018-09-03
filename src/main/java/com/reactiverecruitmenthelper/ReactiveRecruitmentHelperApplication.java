1b98a55e7fac7c803b239927a6953f3b5c37659e
Jakub Stanislawczyk (PGS Software) <jstanislawczyk@pgs-soft.com>
2018-09-03 09:14:16 +0200

    Initial commit

---
+++ src/main/java/com/reactiverecruitmenthelper/ReactiveRecruitmentHelperApplication.java
@@ -0,0 +1,12 @@
+package com.reactiverecruitmenthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRecruitmentHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveRecruitmentHelperApplication.class, args);
+    }
+}
