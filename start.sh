efbb4a45a334f61f13983ab0374c1a807cea5957
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-09-20 12:44:07 +0200

    Small changes in README

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ printf "####################################\n"
 printf "STOP AND REMOVE CONTAINERS         #"
 printf "\n####################################\n\n"
 
-printf "\nStop and remove spring app\n"
+printf "Stop and remove spring app\n"
 docker stop reactive-recruitment-helper
 docker rm reactive-recruitment-helper
 
@@ -12,7 +12,7 @@ printf "\nstop and remove mongo database\n"
 docker stop reactive-mongo
 docker rm reactive-mongo
 
-printf "\n\n####################################\n"
+printf "\n####################################\n"
 printf "REMOVE IMAGES                      #"
 printf "\n####################################\n\n"
 
@@ -25,26 +25,26 @@ docker rmi $(docker images --format '{{.Repository}}' | grep 'reactive-mongo')
 printf "\nremove openjdk image\n"
 docker rmi $(docker images --format '{{.Repository}}' | grep 'openjdk')
 
-printf "\n\n####################################\n"
+printf "\n####################################\n"
 printf "PULL IMAGES                        #"
 printf "\n####################################\n\n"
 
-printf "\npull openjdk\n"
+printf "pull openjdk\n"
 docker pull openjdk:10
 
-printf "\npull mongo\n"
+printf "\npull mongo"
 docker pull mongo
 
-printf "\n\n####################################\n"
+printf "\n####################################\n"
 printf "BUILD FROM DOCKERFILE              #"
 printf "\n####################################\n\n"
 docker build -f Dockerfile -t reactive-recruitment-helper .
 
-printf "\n\n####################################\n"
+printf "\n####################################\n"
 printf "RUN CONTAINERS                     #"
 printf "\n####################################\n\n"
 
-printf "\nrun mongo\n"
+printf "run mongo\n"
 docker run --name reactive-mongo -p 27018:27017 -d mongo
 
 printf "\nrun spring app\n"
